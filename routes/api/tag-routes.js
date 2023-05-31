a6c50a061741a90d77d49bfe8896efa4043d4290
Lauren Wollaston <laurenwollaston@outlook.com>
2023-05-30 21:23:24 -0500

    can put and delte tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -50,16 +50,54 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    var { tag_name } = req.body;
+    const existingTag = await Tag.findOne({ where: { tag_name } });
+    if (existingTag) {
+      res.status(400).json({ message: 'Tag already exists.' });
+      return;
+    }
+    const newTag = await Tag.create({ tag_name });
+    res.redirect('/api/tags/')
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server Error'});
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  // update a tag by its `id` value
+  try {
+    var { id } = req.params;
+    var { tag_name } = req.body;
+    const tag = await Tag.findOne({where: { id }});
+    if (!tag) {
+      return res.status(404).json({ error: 'Tag not found' });
+    };
+    tag.tag_name = tag_name;
+    await tag.save();
+    res.json({ message: 'Tag updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: 'Server Error'});
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const tag = await Tag.findByPk(id);
+    if (!tag) {
+      return res.status(404).json({ error: 'Tag not found' });
+    }
+    await ProductTag.destroy({ where: { tag_id: id } });
+    await tag.destroy();
+    res.json({ message: 'Tag deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server Error' });
+  }
 });
 
 module.exports = router;

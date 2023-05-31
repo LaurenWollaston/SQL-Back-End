d449babba5f10d8a332f2060e77b849ffc98c225
Lauren Wollaston <laurenwollaston@outlook.com>
2023-05-30 21:54:35 -0500

    Revised products update so that is uses async

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -109,45 +109,34 @@ router.post('/', async (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
-  // update product data
-  Product.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((product) => {
-      // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
-    })
-    .then((productTags) => {
-      // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
-        });
-      // figure out which ones to remove
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
-
-      // run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
-    })
-    .then((updatedProductTags) => res.json(updatedProductTags))
-    .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
-    });
+router.put('/:id', async (req, res) => {
+  try {
+    var { id } = req.params;
+    var { product_name, price, stock, category_id, tagIds } = req.body;
+    const product = await Product.findOne({where: { id }});
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    };
+    product.product_name = product_name;
+    product.price = price;
+    product.stock = stock;
+    product.category_id = category_id;
+    await ProductTag.destroy({ where: { product_id: id } });
+    if (tagIds.length) {
+      const productTagIdArr = tagIds.map((tag_id) => {
+        return {
+          product_id: id,
+          tag_id,
+        };
+      });
+      await ProductTag.bulkCreate(productTagIdArr);
+    }
+    await product.save();
+    res.json({ message: 'Product updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: 'Server Error'});
+  }
 });
 
 router.delete('/:id', async (req, res) => {

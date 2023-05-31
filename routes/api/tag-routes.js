92c57cb0e743e1b3d1ed13090418b79d523f081b
Lauren Wollaston <laurenwollaston@outlook.com>
2023-05-30 21:04:20 -0500

    working tags get route.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,27 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tags = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+          through: {
+            model: ProductTag,
+            attributes: [] // Exclude join table attributes
+          },
+          attributes: ['id', 'product_name', 'stock', 'price'],
+          as: 'products'
+        },
+      ],
+    });
+
+    res.json(tags);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server Error' });
+  }
 });
 
 router.get('/:id', (req, res) => {
